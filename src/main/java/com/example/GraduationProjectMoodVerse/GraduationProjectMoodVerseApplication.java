a7387cf65c58024a397a73ad2881fddb23e2e01d
IrinaMinkova <irina.minkova88@gmail.com>
2019-03-19 11:29:50 +0100

    Creates the Spring project

---
+++ src/main/java/com/example/GraduationProjectMoodVerse/GraduationProjectMoodVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.GraduationProjectMoodVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProjectMoodVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationProjectMoodVerseApplication.class, args);
+	}
+
+}
